505eb63a0f09ca20a50e086668f0978dd78e99d6
Noah Vassards <noah.vassards@epitech.eu>
2023-12-06 14:12:13 +0100

    fix(sparql): remove useless attribute query

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -11,7 +11,7 @@ const sparqlQueryMovie = (movieTitle) => `
     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
     PREFIX dbo: <http://dbpedia.org/ontology/>
     
-    SELECT DISTINCT ?movie ?title ?director (GROUP_CONCAT(?date; separator=", ") AS ?dates) (GROUP_CONCAT(?actor; separator=", ") AS ?actors)
+    SELECT DISTINCT ?movie ?title ?director (GROUP_CONCAT(?actor; separator=", ") AS ?actors)
     WHERE {
       ?movie rdf:type dbo:Film;
              rdfs:label ?title;
